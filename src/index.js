e162b06eb70b691d3d5ac93d6ccb6d9d62fea167
m.hosseini <mandan.abolhosseini2@gmail.com>
2019-10-06 17:54:45 +0330

    Create

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap-v4-rtl/dist/css/bootstrap-rtl.css';
+import 'font-awesome/css/font-awesome-css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
